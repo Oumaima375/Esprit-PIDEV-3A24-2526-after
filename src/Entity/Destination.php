3f8dad2e522af518adb7dbd021dd07655cc44feb
Oumaima375 <chebbi.oumaima1@esprit.tn>
2026-04-05 10:37:47 +0100

    modification de destination

--- src/Entity/Destination.php
+++ src/Entity/Destination.php
@@ -3,120 +3,130 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
 use Doctrine\Common\Collections\Collection;
-use App\Entity\Voyage;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity]
 class Destination
 {
-
     #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
     #[ORM\Column(type: "integer")]
-    private int $id_destination;
+    private ?int $id_destination = null;
 
     #[ORM\Column(type: "string", length: 100)]
-    private string $pays;
+    #[Assert\NotBlank(message: 'Le pays est obligatoire')]
+    private ?string $pays = null;
 
     #[ORM\Column(type: "string", length: 100)]
-    private string $ville;
+    #[Assert\NotBlank(message: 'La ville est obligatoire')]
+    private ?string $ville = null;
 
     #[ORM\Column(type: "string", length: 50)]
-    private string $continent;
+    #[Assert\NotBlank(message: 'Le continent est obligatoire')]
+    private ?string $continent = null;
 
-    #[ORM\Column(type: "text")]
-    private string $description;
+    #[ORM\Column(type: "text", nullable: true)]
+    private ?string $description = null;
 
-    #[ORM\Column(type: "string", length: 255)]
-    private string $image;
-
-    public function getId_destination()
-    {
-        return $this->id_destination;
-    }
-
-    public function setId_destination($value)
-    {
-        $this->id_destination = $value;
-    }
-
-    public function getPays()
-    {
-        return $this->pays;
-    }
-
-    public function setPays($value)
-    {
-        $this->pays = $value;
-    }
-
-    public function getVille()
-    {
-        return $this->ville;
-    }
-
-    public function setVille($value)
-    {
-        $this->ville = $value;
-    }
-
-    public function getContinent()
-    {
-        return $this->continent;
-    }
-
-    public function setContinent($value)
-    {
-        $this->continent = $value;
-    }
-
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    public function setDescription($value)
-    {
-        $this->description = $value;
-    }
-
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    public function setImage($value)
-    {
-        $this->image = $value;
-    }
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $image = null;
 
     #[ORM\OneToMany(mappedBy: "id_destination", targetEntity: Voyage::class)]
     private Collection $voyages;
 
-        public function getVoyages(): Collection
-        {
-            return $this->voyages;
+    public function __construct()
+    {
+        $this->voyages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id_destination;
+    }
+
+    public function getId_destination(): ?int
+    {
+        return $this->id_destination;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(?string $pays): self
+    {
+        $this->pays = $pays;
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(?string $ville): self
+    {
+        $this->ville = $ville;
+        return $this;
+    }
+
+    public function getContinent(): ?string
+    {
+        return $this->continent;
+    }
+
+    public function setContinent(?string $continent): self
+    {
+        $this->continent = $continent;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function getVoyages(): Collection
+    {
+        return $this->voyages;
+    }
+
+    public function addVoyage(Voyage $voyage): self
+    {
+        if (!$this->voyages->contains($voyage)) {
+            $this->voyages[] = $voyage;
+            $voyage->setId_destination($this);
         }
-    
-        public function addVoyage(Voyage $voyage): self
-        {
-            if (!$this->voyages->contains($voyage)) {
-                $this->voyages[] = $voyage;
-                $voyage->setId_destination($this);
+        return $this;
+    }
+
+    public function removeVoyage(Voyage $voyage): self
+    {
+        if ($this->voyages->removeElement($voyage)) {
+            if ($voyage->getId_destination() === $this) {
+                $voyage->setId_destination(null);
             }
-    
-            return $this;
         }
-    
-        public function removeVoyage(Voyage $voyage): self
-        {
-            if ($this->voyages->removeElement($voyage)) {
-                // set the owning side to null (unless already changed)
-                if ($voyage->getId_destination() === $this) {
-                    $voyage->setId_destination(null);
-                }
-            }
-    
-            return $this;
-        }
+        return $this;
+    }
 }
